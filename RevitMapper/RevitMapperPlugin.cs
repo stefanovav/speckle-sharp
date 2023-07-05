002a93596a27e0e6edd7d128e8419ab765f026d5
oguzhankoral <oguzhankoral@gmail.com>
2023-07-05 19:00:45 +0300

    Build plugin with .spl extension

--- RevitMapper/RevitMapperPlugin.cs
+++ RevitMapper/RevitMapperPlugin.cs
@@ -1,14 +1,18 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Speckle.Core.Plugins;
 
 namespace RevitMapper
 {
   public class RevitMapperPlugin : SpecklePlugin
   {
+    public override string Name => "Revit Mapper";
+
+    public override Guid Id => typeof(RevitMapperView).GUID;
+
+    public override string Author => "";
+
+    public override string Url => "";
+
     public override IView OnLoad(IApp speckleApp)
     {
       return new RevitMapperView(speckleApp);
