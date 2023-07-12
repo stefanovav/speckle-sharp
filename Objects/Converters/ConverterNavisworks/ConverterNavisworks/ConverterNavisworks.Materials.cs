eea3034f04767b3f2bf01223e7fd4e7087db5eb4
Jonathon Broughton <jonathon@stardotbmp.com>
2023-07-12 10:05:03 +0100

    material conversion logic
    - Added a new method `NavisworksColorToColor` to convert Navisworks color dictionary to System.Drawing.Color
    - Modified the `TranslateMaterial` method to handle different color modes and create the render material accordingly
    - Extracted the logic for converting Navisworks render materials to Speckle render materials into a separate method `NavisworksRenderMaterialToSpeckle`
    - Created a helper method `ColorToInt` to convert Color objects to integer representation
    - Added a new method `CategoriseMaterialProperties` to categorize material properties into a structured dictionary

--- Objects/Converters/ConverterNavisworks/ConverterNavisworks/ConverterNavisworks.Materials.cs
+++ Objects/Converters/ConverterNavisworks/ConverterNavisworks/ConverterNavisworks.Materials.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Autodesk.Navisworks.Api;
+using Autodesk.Navisworks.Gui.Common.View;
 using Objects.Other;
 using Color = System.Drawing.Color;
 
@@ -18,6 +19,25 @@ public partial class ConverterNavisworks
     );
   }
 
+  private static Color NavisworksColorToColor(IReadOnlyDictionary<string, DataProperty> color)
+  {
+    Color colorFromArgb = new();
+    try
+    {
+      colorFromArgb = Color.FromArgb(
+        Convert.ToInt32(color["Red"].Value.ToDouble() * 255),
+        Convert.ToInt32(color["Green"].Value.ToDouble() * 255),
+        Convert.ToInt32(color["Blue"].Value.ToDouble() * 255)
+      );
+    }
+    catch (NullReferenceException)
+    {
+      _ = 0;
+    }
+
+    return colorFromArgb;
+  }
+
   private static RenderMaterial TranslateMaterial(ModelItem geom)
   {
     // Already there anticipating other options becoming possible and set in the settings.
@@ -27,17 +47,14 @@ public partial class ConverterNavisworks
     if (hasColorMode == false || colorMode == null)
       colorMode = "rendered";
 
-    if (colorMode == "rendered")
-    {
-      RenderMaterial renderMaterial = NavisworksRenderMaterialToSpeckle(geom);
-    }
+    RenderMaterial renderMaterial;
 
     Color renderColor = colorMode switch
     {
       "original" => NavisworksColorToColor(geom.Geometry.OriginalColor),
       "active" => NavisworksColorToColor(geom.Geometry.ActiveColor),
       "permanent" => NavisworksColorToColor(geom.Geometry.PermanentColor),
-      _ => new Color()
+      _ => NavisworksColorToColor(geom.Geometry.OriginalColor)
     };
 
     double transparency = colorMode switch
@@ -45,12 +62,10 @@ public partial class ConverterNavisworks
       "original" => geom.Geometry.OriginalTransparency,
       "active" => geom.Geometry.ActiveTransparency,
       "permanent" => geom.Geometry.PermanentTransparency,
-      _ => 0
+      _ => geom.Geometry.OriginalTransparency
     };
 
-    var materialName = $"NavisworksMaterial_{Math.Abs(renderColor.ToArgb())}";
-
-    var black = Color.FromArgb(Convert.ToInt32(0), Convert.ToInt32(0), Convert.ToInt32(0));
+    var materialName = $"NavisworksMaterial_{ColorToInt(renderColor)}";
 
     var itemCategory = geom.PropertyCategories.FindCategoryByDisplayName("Item");
     if (itemCategory != null)
@@ -70,21 +85,95 @@ public partial class ConverterNavisworks
         materialName = name.Value.ToDisplayString();
     }
 
-    var r = new RenderMaterial(1 - transparency, 0, 1, renderColor, black) { name = materialName };
+    if (colorMode == "rendered")
+    {
+      renderMaterial = NavisworksRenderMaterialToSpeckle(geom);
+    }
+    else
+    {
+      var black = Color.FromArgb(Convert.ToInt32(0), Convert.ToInt32(0), Convert.ToInt32(0));
+      renderMaterial = new RenderMaterial(1 - transparency, 0, 1, renderColor, black) { name = materialName };
+    }
 
-    return r;
+    return renderMaterial;
   }
 
   private static RenderMaterial NavisworksRenderMaterialToSpeckle(ModelItem item)
   {
     var renderMaterial = new RenderMaterial();
-    var itemCategory = item.PropertyCategories.FindCategoryByDisplayName("Item");
-    var itemProperties = itemCategory?.Properties;
-    var itemMaterial = itemProperties?.FindPropertyByDisplayName("Material");
-    
     var materialPropertyCategory = item.PropertyCategories.FindCategoryByDisplayName("Material");
     var materialProperties = materialPropertyCategory?.Properties;
+    if (materialProperties == null)
+      return null;
+
+    var properties = CategoriseMaterialProperties(materialProperties);
+
+    var diffuse = NavisworksColorToColor(properties["Diffuse"]);
+    var ambient = NavisworksColorToColor(properties["Ambient"]);
+
+    Color combined;
+
+    combined = Color.FromArgb(
+      Convert.ToInt32((diffuse.R + ambient.R) / 2),
+      Convert.ToInt32((diffuse.G + ambient.G) / 2),
+      Convert.ToInt32((diffuse.B + ambient.B) / 2)
+    );
+
+    renderMaterial.diffuse = ColorToInt(combined);
+    // renderMaterial.diffuse = ColorToInt(NavisworksColorToColor(properties["Diffuse"]));
+    renderMaterial.emissive = ColorToInt(NavisworksColorToColor(properties["Emissive"]));
+
+    renderMaterial.metalness =
+      (
+        properties["Specular"]["Red"].Value.ToDouble()
+        + properties["Specular"]["Green"].Value.ToDouble()
+        + properties["Specular"]["Blue"].Value.ToDouble()
+      ) / 3;
+
+    renderMaterial.opacity = 1 - properties["Special"]["Transparency"].Value.ToDouble();
+    renderMaterial.roughness = 1 - properties["Special"]["Shininess"].Value.ToDouble();
 
     return renderMaterial;
+  }
+
+  private static int ColorToInt(Color color)
+  {
+    var abs = Math.Abs(color.ToArgb());
+    var i = color.ToArgb();
+
+    return i;
+  }
+
+  /// <summary>
+  /// Categorises material properties into a structured dictionary.
+  /// </summary>
+  /// <param name="materialProperties">The collection of material properties to categorise.</param>
+  /// <returns>A nested dictionary where the outer dictionary's keys are categories ("Ambient", "Diffuse", "Specular", "Emissive", and "Special"),
+  /// and the inner dictionary's keys are property names ("Red", "Green", "Blue", "Shininess", and "Transparency").</returns>
+  private static Dictionary<string, Dictionary<string, DataProperty>> CategoriseMaterialProperties(
+    DataPropertyCollection materialProperties
+  )
+  {
+    var properties = new Dictionary<string, Dictionary<string, DataProperty>>();
+    var categories = new[] { "Ambient", "Diffuse", "Specular", "Emissive" };
+    var propertyNames = new[] { "Red", "Green", "Blue" };
+
+    foreach (var category in categories)
+    {
+      properties[category] = new Dictionary<string, DataProperty>();
+      foreach (var name in propertyNames)
+      {
+        properties[category][name] = materialProperties?.FindPropertyByDisplayName($"{category}.{name}");
+      }
+    }
+
+    // Special cases
+    properties["Special"] = new Dictionary<string, DataProperty>()
+    {
+      { "Shininess", materialProperties?.FindPropertyByDisplayName("Shininess") },
+      { "Transparency", materialProperties?.FindPropertyByDisplayName("Transparency") }
+    };
+
+    return properties;
   }
 }
